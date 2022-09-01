efd90b7267f898af51384d65784548366b93c651
appu2006 <93041202+appu2006@users.noreply.github.com>
2022-09-01 14:16:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import os
 
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 19134212))
+    APP_ID = int(os.environ.get("APP_ID", "19134212"))
     API_HASH = os.environ.get("API_HASH", "0d85b66bf650d9d64ec178e51fa6b2c9")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5467887095:AAF0aAq7sErI1I3prL9-mj8MO9xJRp_aCy8")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1239693367 5588560522 5540091710").split())
-    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT") in ["true"]
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL")) if os.environ.get("-1001657271959") else None
+    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT", "true")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL","-1001657271959")) 
     # DON'T CHANGE THESE 2 VARS
     DOWNLOAD_LOCATION = "./ARKJ_Bots"
     TG_MAX_SIZE = 2040108421
